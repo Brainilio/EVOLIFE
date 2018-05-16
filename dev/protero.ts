46b3b061e1a14d95fce1f861a4ff129e4faccfa7
B.A. Imambaks Rodrigues <0924777@hr.nl>
2018-05-16 13:31:54 +0200

    aaaa

--- dev/protero.ts
+++ dev/protero.ts
@@ -1,10 +1,15 @@
 class Protero { 
 
-    private element:HTMLElement
+    private element:HTMLElement;
+    
 
     constructor() { 
         this.element = document.createElement("protero")
         document.body.appendChild(this.element)
+        
+      
+
+
     }
     public update(){ 
         this.element.style.transform = `translate(100px, 100px)`
